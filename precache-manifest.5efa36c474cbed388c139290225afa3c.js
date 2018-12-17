35a5947c14adfca1066293dfcf0a077241244aa9
LAPTOP-NQL8NABP\yosef <yosefwuveh@gmail.com>
2018-12-17 19:17:19 +0200

    Updates

---
+++ precache-manifest.5efa36c474cbed388c139290225afa3c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1e075168e59221e41aa0",
+    "url": "/countries-list-test/static/js/runtime~main.1e075168.js"
+  },
+  {
+    "revision": "3ad06354a69307e87f8b",
+    "url": "/countries-list-test/static/js/main.3ad06354.chunk.js"
+  },
+  {
+    "revision": "6f53afed475923277732",
+    "url": "/countries-list-test/static/js/1.6f53afed.chunk.js"
+  },
+  {
+    "revision": "3ad06354a69307e87f8b",
+    "url": "/countries-list-test/static/css/main.e54e2c36.chunk.css"
+  },
+  {
+    "revision": "6b5ec283d9c493f3acbf345aad7b701f",
+    "url": "/countries-list-test/index.html"
+  }
+];
